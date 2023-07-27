92b726e1e84cb9d6578b585f16faa6a00898a174
Alexander Mamlygo <alexander.mamlygo@gmail.com>
2023-07-27 11:54:37 +0400

    Initial Commit

---
+++ PathAndAnimation/PathAndAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathAndAnimationApp.swift
+//  PathAndAnimation
+//
+//  Created by Aleksandr Mamlygo on 27.07.23.
+//
+
+import SwiftUI
+
+@main
+struct PathAndAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
